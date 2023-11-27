93b663972cb2037878ee8725d2065594dfca9f1d
Thor Whalen <thorwhalen1@gmail.com>
2023-11-27 12:02:42 +0100

    feat: reload_module

--- qo/__init__.py
+++ qo/__init__.py
@@ -6,6 +6,23 @@ from qo.qo_utils import module_not_found_ignore, find_objects
 
 source_module_names = ['tw']
 
+
+def reload_module(module):
+    """Reload a module"""
+    from types import ModuleType
+    from importlib import reload as _reload, import_module
+
+    if not isinstance(module, ModuleType):
+        if isinstance(module, str):
+            module = import_module(module)
+        elif hasattr(module, '__module__'):
+            module = import_module(module.__module__)
+        else:
+            raise TypeError(f'Expected module or module name: {module}')
+
+    return _reload(module)
+
+
 # TODO: automate the following from source_module_names specs
 with module_not_found_ignore:
     from qo.tw import *
@@ -36,4 +53,3 @@ def find_names(query: str):
             query, _root_names, key=[_methodcaller('span'), lambda x: len(x.string)]
         )
     )
-
